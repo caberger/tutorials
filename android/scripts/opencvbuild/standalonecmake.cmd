a133c5310a9d8cb2350029a9e2fe954aa016d01b
qrzbrz <qrzbrz@yahoo.de>
2017-03-19 15:37:41 +0100

    removed duplicate script

--- android/scripts/opencvbuild/standalonecmake.cmd
+++
@@ -1,9 +0,0 @@
-SET ABI=x86_64
-rm -r build
-mkdir build
-pushd build
-
-SET CMAKE_PROG="%ANDROID_SDK%\cmake\3.6.3155560\bin\cmake.exe"
-"C:\Android\sdk\cmake\3.6.3155560\bin\cmake.exe" -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%OPENCV_SOURCE_CODE_DIR%/install -DANDROID_ABI=%ABI% -G"Android Gradle - Ninja" -DANDROID_PLATFORM=android-24 -DCMAKE_TOOLCHAIN_FILE=%NDK%\build\cmake\android.toolchain.cmake -DANDROID_NDK=%NDK% -DCMAKE_MAKE_PROGRAM=%ANDROID_SDK%\cmake\3.6.3155560\bin\ninja.exe ..
-cmake --build . --target install -- -j4
-popd
